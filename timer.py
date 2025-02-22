619147171ef5a31118a1e64e3fdb539d6592f187
Mallikarjun Jamadarkhani <jamadarkhani29.maj@gmail.com>
2025-02-22 23:59:08 +0530

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,7 +1,8 @@
 import time
-!pip install numpy
+import subprocess
+import sys
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy']) 
 import numpy
-
 print(time.time())
 arr = np.array([1,2,3])
 print(arr)
